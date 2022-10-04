f5ec4ccf1dc09afcaa6224de59456a6bfca7bab2
GUSEW <ilaygysew1993@gmail.com>
2022-10-04 23:34:57 +0300

    выполнили вторую задачу

---
+++ zad2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Clear();
+Console.WriteLine("Введите первое число" );
+int x = int.Parse(Console.ReadLine() !);
+Console.WriteLine("Введите второе число:" );
+int y = int.Parse(Console.ReadLine() !);
+if (x > y)
+{
+    Console.WriteLine( x);
+}
+if (x < y)
+{
+    Console.WriteLine(y);
+}
