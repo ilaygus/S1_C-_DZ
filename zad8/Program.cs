8b9ed00b82de20f02f812e25d46260c5851b6c6a
GUSEW <ilaygysew1993@gmail.com>
2022-10-06 00:58:04 +0300

    задание 8

---
+++ zad8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine()!); 
+int index = 1;
+
+while (index <= x) 
+{
+if (index % 2 == 0) 
+{
+Console.Write($"{index} "); 
+}
+index++; 
+}
