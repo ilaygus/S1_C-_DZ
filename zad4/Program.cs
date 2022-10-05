380275df3ae6424ea018664d439684e919e42382
GUSEW <ilaygysew1993@gmail.com>
2022-10-05 23:32:06 +0300

    выполнили 4задачу

--- zad4/Program.cs
+++ zad4/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+int max;
+
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine()!);
+
+if (number1 > number2)
+{
+max = number1;
+}
+else
+{
+max = number2;
+}
+if (number3 > max)
+{
+max = number3;
+}
+Console.Write($"максимальное число {max} ");
