b94a739324b8b8266e5d666cb4fd32b727c21298
GUSEW <ilaygysew1993@gmail.com>
2022-10-10 18:52:22 +0300

    задание 13

---
+++ #2/zad13.1/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+if (Math.Abs(number) < 100)
+{
+Console.Write("третьей цифры нет");
+}
+else
+{
+if (Math.Abs(number) > 999)
+while (Math.Abs(number) > 999)
+{
+number = number / 10;
+}
+Console.Write(Math.Abs(number) % 10);
+}
