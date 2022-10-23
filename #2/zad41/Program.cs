35ff2e04c40eb55910f5ea0cd6b1726b424f48b3
GUSEW <ilaygysew1993@gmail.com>
2022-10-24 00:42:58 +0300

    zadanie41

---
+++ #2/zad41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] GetArray(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] res = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    foreach (int el in array)
+    {
+        if (el > 0) count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine("Введите числа через пробел: ");
+string input = Console.ReadLine()!;
+int[] numArray = GetArray(input);
+int count = CountPositive(numArray);
+Console.WriteLine($"Чисел больше нуля: {count}");
