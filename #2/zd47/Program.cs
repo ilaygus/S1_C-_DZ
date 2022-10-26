fa3e4534d7f977ed0e024196c6f8d433becb26e2
GUSEW <ilaygysew1993@gmail.com>
2022-10-27 00:02:20 +0300

    zadavie47

---
+++ #2/zd47/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] GetArray(int m, int n, double minValue, double maxValue)
+{
+    double[,] result = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+        }
+
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]:f2}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int row = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(row, columns, 0, 10);
+PrintArray(array);
