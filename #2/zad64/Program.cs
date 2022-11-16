ac0120b4e53edc3fe80bfe9bb2eb0b3142286620
GUSEW <ilaygysew1993@gmail.com>
2022-11-17 02:02:19 +0300

    zad64

---
+++ #2/zad64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет 
+//все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+string GetNatural(int n)
+{
+    if (n == 1) 
+    {
+        return $"{1}";
+    }
+    return $"{n}, " + GetNatural(n - 1);
+}
+
+Console.Clear();
+Console.Write("Задайте значение N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Clear();
+Console.WriteLine($"N = {n} -> \"{GetNatural(n)}\"");
