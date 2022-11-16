4bc269bb0bc6641b2b72c64f963ce5511685c3b1
GUSEW <ilaygysew1993@gmail.com>
2022-11-17 02:10:36 +0300

    zad66

---
+++ #2/zad66/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. 
+//Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int GetSum(int m, int n)
+{
+    if (m == n) 
+    {
+        return n;
+    }
+    if (m > n) 
+    {
+        return m + GetSum(m - 1, n);
+    }
+    return m + GetSum(m + 1, n);
+}
+
+Console.Clear();
+Console.Write("Задайте значение M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Задайте значение N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.Clear();
+Console.WriteLine($"M = {m}; N = {n} -> {GetSum(m, n)}");
